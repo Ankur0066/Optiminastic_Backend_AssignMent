64491a0c64ca4b3ed4dc05054c21eb31eeb960f9
MohakMehtaCIMTDEV <mohak.m@canimagedev.in>
2026-01-09 14:32:45 +0530

    Folder structuing

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import userRoutes from "./routes/user.routes";
-import authRoutes from "./routes/auth.routes";
-import masterRoutes from "./routes/master.routes";
 import { getDbPool } from "./config/dbconfig";
+import globalRoute from "./globalRoute";
 
 dotenv.config();
 
@@ -36,10 +34,9 @@ app.get("/health", (_req: Request, res: Response) => {
   res.send("Server is running 🚀");
 });
 
-//  routes
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/master", masterRoutes);
+//  global route calling
+
+app.use("/api",globalRoute)
 
 /* -------------------- SERVER START -------------------- */
 
